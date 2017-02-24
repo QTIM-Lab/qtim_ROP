99cdc89d0f4a30f7f75f6bed0d4c1193011201f2
James Brown <jbrown97@mgh.harvard.edu>
2017-02-24 11:07:43 -0500

    Code to generate two class txt file for DIGITs

---
+++ two_class.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+from os.path import join
+from os import listdir
+
+COMBINED_CLASSES = {'Plus': '1', 'Pre-Plus': '1', 'No': '0'}
+
+
+def two_class(data_path):
+
+    txt_out = join(data_path, 'two_class.txt')
+
+    with open(txt_out, 'w') as txt_file:
+
+        for class_name, class_id in COMBINED_CLASSES.iteritems():
+
+            class_dir = join(data_path, class_name)
+            class_num = COMBINED_CLASSES[class_name]
+
+            print class_dir
+
+            for im_path in listdir(class_dir):
+
+                full_path = join(class_dir, im_path)
+                # full_path = full_path.replace(...)
+                txt_file.write('{} {}\n'.format(full_path, class_num))
+
+
+if __name__ == '__main__':
+
+    import sys
+    data_path = sys.argv[1]
+    two_class(data_path)
