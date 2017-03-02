b751ae749c2fbf8068b799310d495dd6c7d4edbb
James Brown <jbrown97@mgh.harvard.edu>
2017-03-02 11:02:56 -0500

    Got pre-trained ResNet running on a small dataset

--- train_resnet.py
+++ train_resnet.py
@@ -1,13 +1,15 @@
-from os import mkdir, listdir
+from os import listdir
 from os.path import dirname, basename, splitext
 
 from keras.models import Sequential
 from keras.layers import Dropout, Flatten, Dense
 from keras.preprocessing.image import ImageDataGenerator
 from keras.utils.visualize_util import plot
+from keras.utils.np_utils import to_categorical
 from resnet50 import ResNet50
 
 from common import *
+
 
 class RetinaResNet(object):
 
@@ -24,61 +26,64 @@ class RetinaResNet(object):
         self.nb_train_samples = len(find_images(join(self.train_dir, '*')))
         self.nb_val_samples = len(find_images(join(self.val_dir, '*')))
 
-
     def train(self):
 
         # Create ResNet
+        print "Loading pre-trained ResNet"
         model = ResNet50(weights='imagenet')
         plot(model, join(self.experiment_dir, 'model_full.png'))
 
-        # Pop the last layer off
+        # Pop the last (Dense) and penultimate (Flatten) layers off
         model.layers.pop()
         plot(model, join(self.experiment_dir, 'model_popped.png'))
 
         # Extract features from training/testing data
-        train_features, nb_train_examples = self.predict(model)
-        val_features, nb_val_examples = self.predict(model, train=False)
+        train_features, train_labels = self.predict(model)
+        val_features, val_labels = self.predict(model, train=False)
 
         # Train our next top model...
-        self.train_top_model(train_features, val_features, nb_train_examples, nb_val_examples)
+        self.train_top_model(train_features, val_features, train_labels, val_labels)
 
     def predict(self, model, train=True):
 
         data_path, suffix = (self.train_dir, 'train') if train else (self.val_dir, 'val')
-
-        datagen = ImageDataGenerator()
-        generator = datagen.flow_from_directory(
-            data_path,
-            target_size=model.input_shape[2:],
-            batch_size=32,
-            class_mode=None,
-            shuffle=False)
-
+        features_out = join(self.experiment_dir, 'cnn_features_{}.npy'.format(suffix))
+        labels_out = join(self.experiment_dir, 'labels_{}.npy'.format(suffix))
         no_examples = len(find_images(join(data_path, '*')))
-        cnn_features = model.predict_generator(generator, no_examples)
-        npy_out = join(self.experiment_dir, 'cnn_features_{}.npy'.format(suffix))
-        np.save(open(npy_out, 'w'), cnn_features)
-        return npy_out, no_examples
 
-    def train_top_model(self, train_path, val_path, nb_train_samples, nb_val_examples, nb_epoch=50):
+        if not (isfile(features_out) and isfile(labels_out)):
 
-        train_data = np.load(open(train_path))
-        train_labels = np.array([0] * (nb_train_samples / self.no_classes) +
-                                [1] * (nb_train_samples / self.no_classes) +
-                                [2] * (nb_train_samples / self.no_classes))
+            datagen = ImageDataGenerator()
+            generator = datagen.flow_from_directory(
+                data_path,
+                target_size=model.input_shape[2:],
+                batch_size=32,
+                class_mode=None,
+                shuffle=False)
 
-        val_data = np.load(open(val_path))
-        val_labels = np.array([0] * (nb_val_examples / self.no_classes) +
-                              [1] * (nb_val_examples / self.no_classes) +
-                              [2] * (nb_val_examples / self.no_classes))
+            print "Performing forward pass to generate CNN features"
+            cnn_features = model.predict_generator(generator, no_examples)
+            labels = generator.classes
+            np.save(open(features_out, 'w'), cnn_features)
+            np.save(open(labels_out, 'w'), labels)
+
+        return features_out, labels_out
+
+    def train_top_model(self, train_path, val_path, train_labels, val_labels, nb_epoch=50):
+
+        # Load data and labels (converting the latter to categorical)
+        train_data, train_labels = np.load(open(train_path)), to_categorical(np.load(open(train_labels)))
+        val_data, val_labels = np.load(open(val_path)), to_categorical(np.load(open(val_labels)))
+
+        print "Train features shape: {}".format(train_data.shape)
 
         model = Sequential()
-        model.add(Flatten(input_shape=train_data.shape[1:]))
-        model.add(Dense(256, activation='relu'))
+        model.add(Dense(256, activation='relu', input_shape=train_data.shape[1:]))
         model.add(Dropout(0.5))
-        model.add(Dense(1, activation='sigmoid'))
+        model.add(Dense(3, activation='sigmoid'))
 
-        model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+        model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+        plot(model, join(self.experiment_dir, 'top_model.png'))
 
         model.fit(train_data, train_labels,
                   nb_epoch=nb_epoch, batch_size=32,
