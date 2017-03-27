9ccf489a525dae72d85737faa06bf652af38a578
James Brown <jbrown97@mgh.harvard.edu>
2017-03-27 16:52:03 -0400

    Changed to dstacking segmented images

--- ensemble_segmentation.py
+++ ensemble_segmentation.py
@@ -51,7 +51,7 @@ class UnetEnsemble(object):
             print seg_images
 
             im_name = basename(seg_images[0])
-            seg_arr = np.asarray([np.asarray(Image.open(seg)) for seg in seg_images])
+            seg_arr = np.dstack([np.asarray(Image.open(seg)) for seg in seg_images])
 
             mean_image = np.round(np.mean(seg_arr, axis=2)).astype(np.uint8)
             Image.fromarray(mean_image).save(join(self.mean_dir, im_name))
