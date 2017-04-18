447443e0066ae26aa8010e8384cdc913b4e50ecc
James Brown <jbrown97@mgh.harvard.edu>
2017-04-18 17:14:32 -0400

    Autoencoder modifications

--- features/autoencoder.py
+++ features/autoencoder.py
@@ -12,7 +12,7 @@ from keras.utils.np_utils import to_categorical
 from keras.optimizers import SGD
 
 
-def main(X, y):
+def main(X, y, channels, normalize=False):
 
     # Split training data
     X_train, X_test = train_test_split(X, test_size=.2)
@@ -20,18 +20,19 @@ def main(X, y):
     X_test = np.asarray(X_test)
 
     # Normalise to 0. - 1.
-    X_train = X_train.astype('float32') / 255.
-    X_test = X_test.astype('float32') / 255.
+    if normalize:
+        X_train = X_train.astype('float32') / 255.
+        X_test = X_test.astype('float32') / 255.
 
     # Train model
     print "Training autoencoder"
-    model = train_autoencoder(X_train, X_test)
+    model = train_autoencoder(X_train, X_test, channels)
 
 
-def train_autoencoder(X_train, X_test):
+def train_autoencoder(X_train, X_test, channels):
 
     # Create an autoencoder
-    model = autoencoder()
+    model = autoencoder(channels=channels)
     model.fit(X_train, X_train, nb_epoch=100, batch_size=12, shuffle=True, validation_data=(X_test, X_test))
     return model
 
@@ -41,6 +42,13 @@ def load_data(img_dir, csv_file):
     # Load images
     img_list = sorted(glob(join(img_dir, '*')))
     X = np.asarray([imresize(np.asarray(Image.open(img)), (256, 256)) for img in img_list])
+
+    if len(X.shape) == 3:
+        X = np.expand_dims(X, axis=3)
+        channels = 1
+    else:
+        channels = 3
+
     X = np.transpose(X, (0, 3, 1, 2))
 
     # Load labels
@@ -50,12 +58,12 @@ def load_data(img_dir, csv_file):
     classes = [class_indices[k] for k in raw_classes]
     y = to_categorical(classes)
 
-    return X, y
+    return X, y, channels
 
 
-def autoencoder():
+def autoencoder(channels=3):
 
-    input_img = Input(shape=(3, 256, 256))
+    input_img = Input(shape=(channels, 256, 256))
 
     x = Conv2D(32, 3, 3, activation='relu', border_mode='same')(input_img)
     x = MaxPooling2D((2, 2), border_mode='same')(x)
@@ -70,17 +78,17 @@ def autoencoder():
     x = UpSampling2D((2, 2))(x)
     x = Conv2D(32, 3, 3, activation='relu', border_mode='same')(x)
     x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(3, 3, 3, activation='sigmoid', border_mode='same')(x)
+    decoded = Conv2D(channels, 3, 3, activation='sigmoid', border_mode='same')(x)
 
     ae = Model(input_img, decoded)
 
     sgd = SGD(lr=0.01, momentum=.9, decay=1e-3)
-    ae.compile(optimizer=sgd, loss='binary_crossentropy')
+    ae.compile(optimizer=sgd, loss='mse')
     return ae
 
 if __name__ == '__main__':
 
     import sys
 
-    imgs, labels = load_data(sys.argv[1], sys.argv[2])
-    main(imgs, labels)
+    imgs, labels, channels = load_data(sys.argv[1], sys.argv[2])
+    main(imgs, labels, channels)
