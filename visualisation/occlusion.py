3152af8445a336981e3e382326b974e7f3d6d240
James Brown <jbrown97@mgh.harvard.edu>
2017-06-05 14:44:05 -0400

    All images loaded at once for inference/occlusion

--- visualisation/occlusion.py
+++ visualisation/occlusion.py
@@ -6,7 +6,11 @@ from utils.common import find_images_by_class
 
 def occlusion_heatmaps(model_config, test_data, out_dir, window_size=12):
 
+    # Load model
+    model = RetiNet(model_config).model
     imgs_by_class = find_images_by_class(test_data)
+
+    img_arr = []
 
     for class_, img_list in imgs_by_class.items():
 
@@ -17,13 +21,14 @@ def occlusion_heatmaps(model_config, test_data, out_dir, window_size=12):
             # Load and prepare image
             img = cv2.imread(img_path)
             img = img.transpose((2, 0, 1))
-            img = np.expand_dims(img, axis=0)
 
-            # Load model
-            model = RetiNet(model_config).model
+            img_arr.append(img)
 
-            x = model.predict(img)
-            print x
+    img_arr = np.stack(img_arr, axis=0)
+    print img_arr.shape
+
+    x = model.predict(img)
+    print x
 
 if __name__ == '__main__':
 
