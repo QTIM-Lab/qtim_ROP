1063f22fccc6fdf96d4221232c014ee8e74af1ea
James Brown <jbrown97@mgh.harvard.edu>
2017-06-05 15:51:31 -0400

    Occlusion heatmaps now generated

--- visualisation/occlusion.py
+++ visualisation/occlusion.py
@@ -1,7 +1,12 @@
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import seaborn as sns
+from os.path import join
 import cv2
 import numpy as np
 from learning.retina_net import RetiNet
-from utils.common import find_images_by_class
+from utils.common import find_images_by_class, make_sub_dir
 
 CLASSES = {0: 'No', 1: 'Plus', 2: 'Pre-Plus'}
 
@@ -14,7 +19,9 @@ def occlusion_heatmaps(model_config, test_data, out_dir, window_size=12):
 
     for class_, img_list in imgs_by_class.items():
 
-        print class_
+        class_dir = make_sub_dir(out_dir, class_)
+
+        no_imgs = len(img_list)
         img_arr = []
 
         for img_path in img_list:
@@ -28,14 +35,37 @@ def occlusion_heatmaps(model_config, test_data, out_dir, window_size=12):
         img_arr = np.stack(img_arr, axis=0)
         print img_arr.shape
 
-        pred = model.predict_on_batch(img_arr)
-        for i, y_pred in enumerate(pred):
+        # Get raw predictions
+        raw_probabilities = model.predict_on_batch(img_arr)
+        raw_predictions = [np.argmax(y_pred) for y_pred in raw_probabilities]
 
-            arg_max = np.argmax(y_pred)
-            pred_class = CLASSES[arg_max]
-            pred_prob = y_pred[arg_max]
+        # Occlude overlapping windows in the image
+        x_dim = img_arr.shape[2]
+        y_dim = img_arr.shape[3]
+        hw = window_size / 2
+        heatmaps = np.zeros(shape=(x_dim, y_dim, no_imgs))
 
-            print "Image #{}: {} (:.2f %)".format(i, pred_class, pred_prob * 100)
+        for x in range(0, x_dim):
+            for y in range(0, y_dim):
+
+                x_range = np.clip(np.arange(x-hw, x+hw), 0, x_dim)
+                y_range = np.clip(np.arange(y-hw, y+hw), 0, y_dim)
+                img_arr[:, :, x_range, y_range] = 0
+
+                # Get predictions for current occluded region
+                occ_probabilites = model.predict_on_batch(img_arr)
+
+                # Assign heatmap value as probability of class, as predicted without occlusion
+                for occ_prob, raw_pred in zip(occ_probabilites, raw_predictions):
+                    heatmaps[x, y] = occ_prob[raw_pred]
+
+        for j, (img, h_map) in enumerate(zip(img_arr, heatmaps)):
+
+            f = plt.figure()
+            plt.imshow(img.tranpose((2, 0, 1)))
+            plt.imshow(h_map, cmap=plt.cm.viridis, alpha=0.7, interpolation='bilinear')
+            plt.savefig(join(class_dir, '{}.png'.format(j)))
+
 
 if __name__ == '__main__':
 
