33893dc042ed880db15d3aa998ee896211724c6d
James Brown <jbrown97@mgh.harvard.edu>
2017-06-09 15:54:37 -0400

    Added option for whether to use RF or not

--- evaluation/ensemble.py
+++ evaluation/ensemble.py
@@ -1,7 +1,7 @@
 from utils.common import make_sub_dir, get_subdirs
 from utils.image import imgs_by_class_to_th_array
 from metrics import confusion
-from learning.retina_rf import RetinaRF, locate_config
+from learning.retina_rf import RetiNet, RetinaRF, locate_config
 from os.path import join
 from glob import glob
 import numpy as np
@@ -9,7 +9,7 @@ import numpy as np
 CLASS_LABELS = {'No': 0, 'Plus': 1, 'Pre-Plus': 2}
 
 
-def evaluate_ensemble(models_dir, test_images, out_dir):
+def evaluate_ensemble(models_dir, test_images, out_dir, rf=False):
 
     # Get images and true classes
     img_arr, y_true = imgs_by_class_to_th_array(test_images, CLASS_LABELS)
@@ -22,12 +22,16 @@ def evaluate_ensemble(models_dir, test_images, out_dir):
 
         # Load model
         print "Loading CNN/RF #{}".format(i)
-        model_config, rf_pkl = locate_config(model_dir)
-        cnn_rf = RetinaRF(model_config, rf_pkl=rf_pkl)
+        if rf:
+            model_config, rf_pkl = locate_config(model_dir)
+            model = RetinaRF(model_config, rf_pkl=rf_pkl)
+        else:
+            config_file = glob(join(model_dir, '*.yaml'))[0]
+            model = RetiNet(config_file)
 
         # Predicted probabilities
         print "Making predictions..."
-        y_preda = cnn_rf.predict(img_arr)
+        y_preda = model.predict(img_arr)
         y_pred_ensemble.append(y_preda)
 
         y_pred = np.argmax(y_preda, axis=1)
@@ -48,8 +52,8 @@ if __name__ == '__main__':
     parser.add_argument('-m', '--models', dest='models', help='Folder of models', required=True)
     parser.add_argument('-i', '--images', dest='images', help='Images to test', required=True)
     parser.add_argument('-o', '--out-dir', dest='out_dir', help='Output directory', required=True)
-
+    parser.add_argument('-r', '--rf', action='store_true', dest=rf, help='Use random forest on CNN features?', default=False)
     args = parser.parse_args()
 
-    evaluate_ensemble(args.models, args.images, args.out_dir)
+    evaluate_ensemble(args.models, args.images, args.out_dir, rf=args.rf)
 
