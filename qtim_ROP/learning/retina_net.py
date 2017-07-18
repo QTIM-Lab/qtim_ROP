f700482db9be97d4078a0b17f7c9974a21eab0db
James Brown <jbrown97@mgh.harvard.edu>
2017-07-18 17:34:28 -0400

    bug fix

--- qtim_ROP/learning/retina_net.py
+++ qtim_ROP/learning/retina_net.py
@@ -185,7 +185,7 @@ class RetiNet(object):
 
         # Plot histories
         plot_accuracy(history, join(self.experiment_dir, 'accuracy' + self.ext),
-                      metric='acc' if self.regression else 'soft_acc')
+                      metric='acc' if not self.regression else 'soft_acc')
         plot_loss(history, join(self.experiment_dir, 'loss' + self.ext))
         plot_LR(lr, join(self.experiment_dir, 'lr_plot' + self.ext))
 
