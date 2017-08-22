76787281001b8dae8c533fc574a5ed1443397d13
James Brown <jbrown97@mgh.harvard.edu>
2017-08-22 10:52:50 -0400

    Few fixes

--- qtim_ROP/webapp/tasks.py
+++ qtim_ROP/webapp/tasks.py
@@ -3,7 +3,9 @@ from flask import Flask, flash, request, redirect, url_for, render_template, ses
 from flask.json import jsonify
 from werkzeug.utils import secure_filename
 import os
+import time
 import numpy as np
+
 from qtim_ROP.__main__ import initialize
 from qtim_ROP.deep_rop import preprocess_images, LABELS
 from qtim_ROP.learning.retina_net import RetiNet, locate_config
@@ -32,7 +34,8 @@ app.config.update(
     CELERY_BROKER_URL='amqp://localhost:6379',
     CELERY_RESULT_BACKEND='amqp://localhost:6379',
     UPLOAD_FOLDER='uploads/raw',
-    OUTPUT_FOLDER='uploads/output'
+    OUTPUT_FOLDER='uploads/output',
+    SEG_FOLDER='uploads/output/segmentation'
 )
 
 celery = make_celery(app)
@@ -65,16 +68,19 @@ def upload():
         filename = secure_filename(file.filename)
         out_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
         file.save(out_path)
-        return render_template("uploaded.html", image_name=filename)
+        return render_template("uploaded.html", image_name=filename, seg_name=None)
 
 
 @app.route('/upload/<filename>')
 def send_original(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
-def send_segmentation(filename):
-    return send_from_directory(os.path.join(app.config['OUTPUT_FOLDER'], 'segmentation'),
-                               os.path.splitext(os.path.basename(filename))[0] + '.bmp')
+
+@app.route('/upload/<seg_name>')
+def send_segmentation(seg_name):
+    print seg_name
+    return send_from_directory(app.config['SEG_FOLDER'], seg_name)
+
 
 @app.route('/longtask', methods=['POST'])
 def longtask():
@@ -87,22 +93,24 @@ def longtask():
 @celery.task(bind=True)
 def long_task(self, filename):
 
-
+    # Initializes the classifer
     self.update_state(state='PROGRESS', meta={'current': 0, 'total': 3, 'status': "Initializing..."})
+    time.sleep(2)
     classifier = RetiNet(model_config)
 
+    # Does the preprocessing
     self.update_state(state='PROGRESS', meta={'current': 1, 'total': 3, 'status': "Preprocessing image..."})
     prep_img, prep_name = preprocess_images([filename], app.config['OUTPUT_FOLDER'],
                                             conf_dict, skip_segmentation=False, batch_size=100, fast=True)
-
     self.update_state(state='PROGRESS', meta={'current': 2, 'total': 3, 'status': "Doing classification..."})
 
+    # Prediction
     y_preda = classifier.predict(prep_img)[0]
     arg_max = np.argmax(y_preda)
     prob = y_preda[arg_max]
     diagnosis = LABELS[arg_max]
 
-    return {'current': 3, 'total': 3, 'status': 'Complete!',
+    return {'current': 3, 'total': 3, 'status': 'Complete!', 'seg_name': prep_name,
             'result': '{} ({:.2f}%)'.format(diagnosis, prob * 100.)}
 
 @app.route('/status/<task_id>')
@@ -126,6 +134,7 @@ def taskstatus(task_id):
         }
         if 'result' in task.info:
             response['result'] = task.info['result']
+            response['seg_name'] = task.info['seg_name']
     else:
         # something went wrong in the background job
         response = {
