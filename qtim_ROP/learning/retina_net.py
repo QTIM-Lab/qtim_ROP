22765d9c0a24e8c980b663386cca50c275ebc95e
James Brown <jbrown97@mgh.harvard.edu>
2018-02-27 18:00:15 -0500

    Added support for different input sizes

--- qtim_ROP/learning/retina_net.py
+++ qtim_ROP/learning/retina_net.py
@@ -116,13 +116,13 @@ class RetiNet(object):
 
             if arch is None:
                 self.model = create_googlenet(network.get('no_classes', 3), network.get('no_features', 128),
-                                              network.get('input_shape', (3, 224, 224)))
+                                              network.get('regression'), network.get('input_shape', (3, 224, 224)))
             else:
                 try:
                     self.model = model_from_json(open(arch).read(), custom_objects=custom_objects)
                 except ValueError:  # keras compatibility issue
-                    self.model = create_googlenet(network.get('no_classes', 3), network.get('no_features', 1024),
-                        network.get('regression'))
+                    self.model = create_googlenet(network.get('no_classes', 3), network.get('no_features', 128),
+                                                  network.get('regression'), network.get('input_shape', (3, 224, 224)))
 
             if weights:
                 print "Loading weights '{}'".format(weights)
