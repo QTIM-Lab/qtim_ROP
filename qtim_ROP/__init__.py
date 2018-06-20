80795467d8fac793f2bc2f0ba97f4c53b176241a
Brown <jbrown97@mgh.harvard.edu>
2018-06-20 14:24:09 -0400

    Bug fix

--- qtim_ROP/__init__.py
+++ qtim_ROP/__init__.py
@@ -3,12 +3,16 @@ matplotlib.use('Agg')
 
 
 from .__main__ import initialize
-conf_dict = initialize()
+conf_dict, _ = initialize()
+
 
 import os  # set the keras backend
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = conf_dict['gpu']
 os.environ['KERAS_BACKEND'] = 'tensorflow'
+
+if conf_dict['gpu'] is not None:
+    os.environ["CUDA_VISIBLE_DEVICES"] = conf_dict['gpu']
+
 import keras
 keras.backend.set_image_dim_ordering('tf')
 
