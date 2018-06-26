c87bdb6f43b96f72b5021fe508c2d9392ce78dd1
Brown <jbrown97@mgh.harvard.edu>
2018-06-26 13:31:28 -0400

    Updated train.py to have custom output dir

--- train.py
+++ train.py
@@ -15,8 +15,9 @@ os.environ["CUDA_VISIBLE_DEVICES"] = gpu
 
 from qtim_ROP.learning.retina_net import RetiNet
 confs = glob(sys.argv[1])
+out_dir = sys.argv[2] if len(sys.argv > 2) else None
 
 for c in confs:
     print(c)
-    net = RetiNet(c)
+    net = RetiNet(c, out_dir=out_dir)
     net.train()
