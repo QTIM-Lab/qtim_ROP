1dab388557d2d91ddfd6ba307cb92d8c86281a2a
Brown <jbrown97@mgh.harvard.edu>
2018-06-27 10:28:54 -0400

    GPU config for training

--- train.py
+++ train.py
@@ -1,17 +1,11 @@
 import sys
 from glob import glob
-
-
-try:
-    gpu = str(sys.argv[2])
-except IndexError:
-    gpu = "0"
-
-print("Training using gpu {}".format(gpu))
+from qtim_ROP.__main__ import initialize
+conf_dict, conf_file = initialize()
 
 import os
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = gpu
+os.environ["CUDA_VISIBLE_DEVICES"] = conf_dict['gpu']
 
 from qtim_ROP.learning.retina_net import RetiNet
 confs = glob(sys.argv[1])
